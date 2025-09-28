dee0f61158db5b5ed0db5cf885a25a1cdf3fb577
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-09-27 22:40:03 -0300

    Trabajando sobre el modal

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,8 +10,33 @@ function crearGaleria() {
     imagen.src = `src/img/gallery/webp/${i + 1}.webp`;
     imagen.alt = "Imagen Galeria";
     imagen.loading = "lazy";
-    imagen.classList.add("galeria-imagen")
+    imagen.classList.add("galeria-imagen");
+
+    imagen.addEventListener("click", () => mostrarModal(i + 1));
     galeriaContainer.appendChild(imagen);
   }
-  console.log("Desde crear galeria");
+}
+
+function mostrarModal(numeroImg) {
+  const galeria = document.querySelector("#galeria");
+  const modal = document.createElement("DIV");
+  modal.classList.add("modal-img");
+
+  const imgModal = document.createElement("IMG");
+  imgModal.src = `src/img/gallery/webp/${numeroImg}.webp`;
+  imgModal.alt = "Imagen Galeria";
+  imgModal.loading = "lazy";
+  modal.appendChild(imgModal);
+
+  const buttonModal = document.createElement("BUTTON");
+  buttonModal.addEventListener("click", cerrarModal);
+  buttonModal.textContent = "X";
+
+  modal.appendChild(buttonModal);
+  galeria.appendChild(modal);
+}
+
+function cerrarModal() {
+  const modal = document.querySelector(".modal-img");
+  modal.remove();
 }
