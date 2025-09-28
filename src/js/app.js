68f6e16f6bb49b8a47000645c04bd431405cf1b9
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-09-27 21:53:53 -0300

    Creando galeria

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,17 @@
-alert("Hola bb");
+document.addEventListener("DOMContentLoaded", function () {
+  crearGaleria();
+});
+
+function crearGaleria() {
+  const galeriaContainer = document.querySelector(".galeria-imagenes");
+
+  for (let i = 0; i < 16; i++) {
+    let imagen = document.createElement("IMG");
+    imagen.src = `src/img/gallery/webp/${i + 1}.webp`;
+    imagen.alt = "Imagen Galeria";
+    imagen.loading = "lazy";
+    imagen.classList.add("galeria-imagen")
+    galeriaContainer.appendChild(imagen);
+  }
+  console.log("Desde crear galeria");
+}
